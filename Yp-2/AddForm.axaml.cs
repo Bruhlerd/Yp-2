c513350293fe31331aed39dea6151e253014f66f
Bruhlerd <clerk_76feed@mail.ru>
2024-03-20 00:14:25 +0700

    Initial commit

---
+++ Yp-2/AddForm.axaml.cs
@@ -0,0 +1,109 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MySql.Data.MySqlClient;
+using System;
+using System.Windows;
+
+
+namespace Yp_2;
+
+public partial class AddEditForm : Window
+{
+    private List<Service> _Services;
+    private Service CurrentServ;
+
+    public AddEditForm(Service currentServ, List<Service> _services)
+    {
+        InitializeComponent();
+        CurrentServ = currentServ;
+        this.DataContext = currentServ;
+        _Services = _services;
+       
+    }
+
+    private MySqlConnection conn;
+    string connStr = "server=localhost; database=yp-02; port=3306; User Id=root;password=Pass&_word1";
+
+    private void Save_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var usr = _Services.FirstOrDefault(x => x.ID == CurrentServ.ID);
+        if (usr == null)
+        {
+            try
+            {
+                conn = new MySqlConnection(connStr);
+                conn.Open();
+                string add =
+                    "INSERT INTO services (name, discount,  duration, price, image_id) VALUES ('" +
+                    Name.Text + "', '" + Convert.ToDouble(discount.Text) + "', '" + duration.Text + "', '" + price.Text + "', '" +
+                    Img.Text + "');";
+                MySqlCommand cmd = new MySqlCommand(add, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                Servic back = new Servic();
+                this.Close();
+                back.Show();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error" + exception);
+            }
+        }
+        else
+        {
+            try
+            {
+                conn = new MySqlConnection(connStr);
+                conn.Open();
+                string upd = "UPDATE services SET name = '" + Name.Text +
+                             "', discount = '" + Convert.ToInt32(discount.Text) + "', duration = '" +
+                             duration.Text + 
+                             "', price = '" + price.Text +  "', image_id = '" +
+                             Img.Text + "' WHERE ID = " + Convert.ToInt32(ID.Text) + ";";
+                MySqlCommand cmd = new MySqlCommand(upd, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                
+                Servic back = new Servic();
+                this.Close();
+                back.Show();
+            }
+            catch (Exception exception)
+            {
+                Console.Write("Error" + exception);
+            }
+        }
+    }
+
+    private void GoBack(object? sender, RoutedEventArgs e)
+    {
+        Servic back = new Servic();
+        this.Close();
+        back.Show();
+    }
+
+    private async void File_Select(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog(); //создание диалогового окна выбора файла
+            fileDialog.Filters.Add(new FileDialogFilter() { Name = "Image Files", Extensions = { "jpg", "jpeg", "png", "gif" } }); //ограничение на выбор только изображений
+            string[]? fileNames = await fileDialog.ShowAsync(this); //отображение диалогового окна и получение выбранных файлов
+            if (fileNames != null && fileNames.Length > 0) //если файл выбран
+            {
+                string imagePath = System.IO.Path.GetFileName(fileNames[0]); //получение пути к выбранному файлу
+                Img.Text = imagePath;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+}
---
+++ Yp-2/Service.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using System.Windows;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Utilities;
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+namespace Yp_2;
+
+public class Service
+{
+    public int ID { get; set; }
+    public string name { get; set; }
+    public int discount { get; set; }
+    public string duration { get; set; }
+    public int price { get; set; }
+    public int price_discount { get; set; }
+    public Bitmap? image_id { get; set; }
+    public string Изображение_услуги_путь { get; set; }
+}
+
+public class Record
+{
+    public int ID { get; set; }
+    public int client_id { get; set; }
+    public int employee_id { get; set; }
+    public int service_id { get; set; }
+    public int date { get; set; }
+}
